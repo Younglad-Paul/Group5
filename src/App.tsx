43156f5d7fa0e01d5a49d5636fdb0b86e80255be
onetruehomie <vicigwilo@gmail.com>
2024-08-15 03:01:31 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import WormholeConnect from '@wormhole-foundation/wormhole-connect';
+
+
+function App() {
+
+  return (
+    <>
+      <WormholeConnect />
+    </>
+  )
+}
+
+export default App
