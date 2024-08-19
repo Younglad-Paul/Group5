cc434ce2cbff7a0c5d39d2af9733bdcab76c5a98
onetruehomie <vicigwilo@gmail.com>
2024-08-19 10:53:25 +0100

    trying to push

--- WormHoleComponents/evmSigner.ts
+++ WormHoleComponents/evmSigner.ts
@@ -1,72 +1,72 @@
-// // import {
-// //   Network,
-// //   SignOnlySigner,
-// //   SignedTx,
-// //   Signer as WormholeSigner,
-// //   UnsignedTransaction,
-// //   PlatformNativeSigner,
-// // } from '@wormhole-foundation/sdk-connect';
-// // import { ethers } from 'ethers';
-// // // import  ChainName  from '@wormhole-foundation/wormhole-connect';
+import {
+  Network,
+  SignOnlySigner,
+  SignedTx,
+  Signer as WormholeSigner,
+  UnsignedTransaction,
+  PlatformNativeSigner,
+} from '@wormhole-foundation/sdk-connect';
+import { ethers } from 'ethers';
+// import  ChainName  from '@wormhole-foundation/wormhole-connect';
 
-// // export type EvmSignerOptions = {
-// //   debug?: boolean;
-// //   maxGasLimit?: bigint;
-// //   overrides?: Partial<ethers.TransactionRequest>;
-// // };
+export type EvmSignerOptions = {
+  debug?: boolean;
+  maxGasLimit?: bigint;
+  overrides?: Partial<ethers.TransactionRequest>;
+};
 
-// // export async function getEvmSigner(
-// //   provider: ethers.Provider,
-// //   signer: ethers.Signer,
-// //   opts?: EvmSignerOptions & { chain?: string },
-// // ): Promise<WormholeSigner> {
-// //   const chain = opts?.chain ?? 'Ethereum';
-// //   const address = await signer.getAddress();
-// //   return new EvmNativeSigner(chain as ChainName, address, signer, opts); // Cannot find name 'ChainName'.ts(2304)
-// // }
+export async function getSigner(
+  provider: ethers.Provider,
+  signer: ethers.Signer,
+  opts?: EvmSignerOptions & { chain?: string },
+): Promise<WormholeSigner> {
+  const chain = opts?.chain ?? 'Ethereum';
+  const address = await signer.getAddress();
+  return new EvmNativeSigner(chain as ChainName, address, signer, opts); // Cannot find name 'ChainName'.ts(2304)
+}
 
-// // export class EvmNativeSigner<N extends Network, C extends ChainName> // Cannot find name 'ChainName'.ts(2304) type ChainName = /*unresolved*/ any
-// //   extends PlatformNativeSigner<ethers.Signer, N, C>
-// //   implements SignOnlySigner<N, C>
-// // {
-// //   constructor(
-// //     _chain: C,
-// //     _address: string,
-// //     _signer: ethers.Signer,
-// //     readonly opts?: EvmSignerOptions,
-// //   ) {
-// //     super(_chain, _address, _signer);
-// //   }
+export class EvmNativeSigner<N extends Network, C extends ChainName> // Cannot find name 'ChainName'.ts(2304) type ChainName = /*unresolved*/ any
+  extends PlatformNativeSigner<ethers.Signer, N, C>
+  implements SignOnlySigner<N, C>
+{
+  constructor(
+    _chain: C,
+    _address: string,
+    _signer: ethers.Signer,
+    readonly opts?: EvmSignerOptions,
+  ) {
+    super(_chain, _address, _signer);
+  }
 
-// //   chain(): C {
-// //     return this._chain;
-// //   }
+  chain(): C {
+    return this._chain;
+  }
 
-// //   address(): string {
-// //     return this._address;
-// //   }
+  address(): string {
+    return this._address;
+  }
 
 
-// //   async sign(tx: UnsignedTransaction<N, C>[]): Promise<SignedTx[]> {
-// //     const signed: SignedTx[] = [];
-// //     for (const txn of tx) {
-// //       const signedTx = await this._signer.signTransaction(txn.transaction as ethers.TransactionRequest);
-// //       signed.push({
-// //         transaction: signedTx,
-// //         signingAddress: this._address,
-// //       });
-// //     }
-// //     return signed;
-// //   }
+  async sign(tx: UnsignedTransaction<N, C>[]): Promise<SignedTx[]> {
+    const signed: SignedTx[] = [];
+    for (const txn of tx) {
+      const signedTx = await this._signer.signTransaction(txn.transaction as ethers.TransactionRequest);
+      signed.push({
+        transaction: signedTx,
+        signingAddress: this._address,
+      });
+    }
+    return signed;
+  }
   
-// // }
+}
 
 
 
 
 
 
-// // ./helpers/index.js
+// ./helpers/index.js
 // import { Wallet } from '@project-serum/anchor';
 // import { Connection } from '@solana/web3.js';
 // import { Algodv2 } from 'algosdk';
